135b17ba9bef053c6f53f3e12f26da2d9f47d871
Lindsey Catlett <lindsey.catlett@gmail.com>
2020-11-12 14:33:40 -0500

    Removed cruft,dupes, and updated homebrew packages.

--- macos/dock.sh
+++ macos/dock.sh
@@ -5,11 +5,8 @@ dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/Sequel Pro.app"
-dockutil --no-restart --add "/Applications/Postman.app"
 dockutil --no-restart --add "/Applications/Slack.app"
-dockutil --no-restart --add "/Applications/TextEdit.app"
 dockutil --no-restart --add "/Applications/Dashlane.app"
-dockutil --no-restart --add "/Applications/Utilities/Activity Monitor.app"
 
 killall Dock
 
